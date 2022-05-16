ee8c3d59bc87071e71bc38e277ea87a138095310
timmsantos <timmsantos@icloud.com>
2022-05-16 13:24:11 +1000

    testing route

--- javascripts/discourse/widgets/category-sidebar.js
+++ javascripts/discourse/widgets/category-sidebar.js
@@ -59,7 +59,7 @@ createWidget("category-sidebar", {
 
     const router = getOwner(this).lookup("router:main");
     const currentRouteParams = router.currentRoute.params;
-    console.log("current route: " + router.currentRoute);
+    console.log("current route: " + currentRouteParams.hasOwnProperty());
     const isCategoryTopicList = currentRouteParams.hasOwnProperty(
       "category_slug_path_with_id"
     );
